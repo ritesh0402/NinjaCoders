e807e39a514f11876a8e5329fb5002a9c21a9c77
ritesh0402 <77856192+ritesh0402@users.noreply.github.com>
2021-09-22 13:49:33 +0530

    ritesh

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# NinjaCoders
+# physics_project
+This is a repository for Physics Project. 
+Project link: https://github.com/ritesh0402/physics_project.git
+Hosted Link: https://ritesh0402.github.io/physics_project/
---
+++ script.js
@@ -0,0 +1,27 @@
+burger = document.querySelector(".burger")
+hamnav = document.querySelector(".hamnav")
+card1 = document.querySelector(".card1");
+card2 = document.querySelector(".card2");
+card3 = document.querySelector(".card3");
+card4 = document.querySelector(".card4");
+
+burger.addEventListener("click", ()=>{
+    hamnav.classList.toggle('hide');
+    // burger.classList.toggle('visible');
+})
+
+card1.addEventListener("click", ()=>{
+    window.location.href = 'https://www.cowin.gov.in/home';
+})
+
+card2.addEventListener("click", ()=>{
+    window.location.href = 'https://ndma.gov.in/';
+})
+
+card3.addEventListener("click", ()=>{
+    window.location.href = 'SubPages/Smit.html';
+})
+
+card4.addEventListener("click", ()=>{
+    window.location.href = 'SubPages/voluntary_help_page.html';
+})
